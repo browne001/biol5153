676e4672314b11582a3812053e122f674813180d
Emily Brown <evb001@uark.edu>
2024-04-01 09:27:35 -0500

    added def()

--- multiply.py
+++ multiply.py
@@ -1,17 +1,32 @@
 #multiply.py
 #! /usr/bin/env python3
-size = int(input("Enter the size of your table: "))
 
-cell_width = len(str((size * size))) + 1
-print(cell_width)
+def get_input():
 
-for i in range(1, size +1):
-    for j in range(1, size + 1):
-        print("{:>{cell_width}}".format(i * j, cell_width = cell_width), end = " ")
+    #ask for size of matrix
+    size = int(input("Enter the size of your table: "))
+    return size
+
+def print_table(n): 
+# calculate the spaces we will need for the output of each cell
+    cell_width = len(str((n * n))) + 1
+    print(cell_width)
+
+#nested for loop
+    for i in range(1, n +1):
+        for j in range(1, n + 1):
+            print("{:>{cell_width}}".format(i * j, cell_width = cell_width), end = " ")
         #print("{:4d}".format(i * j), end = " ")
-    print()
+        print()
 
 
+def main():
+    input_num = get_input()
+
+    print_table(input_num)
+
+if __name__ == '__main__':
+    main()
 
 
 
